fbb54e635d80fe3868c4c0b1254b7e162aa6dd09
techgaun <coolsamar207@gmail.com>
2019-03-20 23:44:15 -0500

    add test deps and setup travis refs #8

--- mix.exs
+++ mix.exs
@@ -7,7 +7,20 @@ defmodule ZXCVBN.MixProject do
       version: "0.1.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+
+      # coverage
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [coveralls: :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test],
+
+      # docs
+      name: "ZXCVBN",
+      source_url: "https://github.com/techgaun/zxcvbn-elixir",
+      homepage_url: "https://github.com/techgaun/zxcvbn-elixir",
+      docs: [
+        main: "ZXCVBN",
+        extras: ["readme.md"]
+      ]
     ]
   end
 
@@ -21,8 +34,10 @@ defmodule ZXCVBN.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:dialyxir, "~> 1.0.0-rc.4", only: [:dev], runtime: false},
+      {:ex_doc, "~> 0.19", only: :dev, runtime: false},
+      {:excoveralls, "~> 0.10.6", only: :test},
+      {:stream_data, "~> 0.1", only: :test}
     ]
   end
 end
--- test/zxcvbn_test.exs
+++ test/zxcvbn_test.exs
@@ -2,7 +2,4 @@ defmodule ZxcvbnTest do
   use ExUnit.Case
   doctest Zxcvbn
 
-  test "greets the world" do
-    assert Zxcvbn.hello() == :world
-  end
 end
