d95ee00e29de43c6a80db9347efc2f459801404a
techgaun <coolsamar207@gmail.com>
2019-03-21 00:04:15 -0500

    initial property testing

--- test/zxcvbn_test.exs
+++ test/zxcvbn_test.exs
@@ -1,4 +1,17 @@
 defmodule ZXCVBNTest do
   use ExUnit.Case
+  use ExUnitProperties
 
+  describe "zxcvbn/1" do
+    property "handles given string" do
+      check all str <- string(:ascii, max_length: 10, min_length: 1),
+                times <- integer(1..20),
+                times >= 0 do
+        %{calc_time: calc_time, password: password, score: score} = ZXCVBN.zxcvbn(str)
+        assert password === str
+        assert calc_time > 0
+        assert score >= 0
+      end
+    end
+  end
 end
