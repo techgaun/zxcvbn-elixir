13f992021b483d754ff1fd56463fe515e68ddfbc
techgaun <coolsamar207@gmail.com>
2019-03-21 20:47:38 -0500

    prepare release

--- README.md
+++ README.md
@@ -7,7 +7,7 @@
 ```elixir
 def deps do
   [
-    {:zxcvbn, "~> 0.1.0"}
+    {:zxcvbn, "~> 0.1.2"}
   ]
 end
 ```
--- test/zxcvbn_test.exs
+++ test/zxcvbn_test.exs
@@ -33,4 +33,12 @@ defmodule ZXCVBNTest do
       end
     end
   end
+
+  test "README.md version is up to date" do
+    app = :zxcvbn
+    app_version = Application.spec(:zxcvbn, :vsn) |> to_string()
+    readme = File.read!("README.md")
+    [_, readme_version] = Regex.run(~r/{:#{app}, "(.+)"}/, readme)
+    assert Version.match?(app_version, readme_version)
+  end
 end
